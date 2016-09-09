7c795d0bfc4b8b3a2baf071be073b22ab6bf3280
Woodane <anjunyou@foxmail.com>
2016-09-09 16:59:17 +0800

    修改key类型，支持int

--- syncmap.go
+++ syncmap.go
@@ -13,7 +13,7 @@ const (
 
 // syncMap wraps built-in map by using RWMutex for concurrent safe.
 type syncMap struct {
-	items map[string]interface{}
+	items map[interface{}]interface{}
 	sync.RWMutex
 }
 
@@ -39,18 +39,23 @@ func NewWithShard(shardCount uint8) *SyncMap {
 	m.shardCount = shardCount
 	m.shards = make([]*syncMap, m.shardCount)
 	for i, _ := range m.shards {
-		m.shards[i] = &syncMap{items: make(map[string]interface{})}
+		m.shards[i] = &syncMap{items: make(map[interface{}]interface{})}
 	}
 	return m
 }
 
 // Find the specific shard with the given key
-func (m *SyncMap) locate(key string) *syncMap {
-	return m.shards[bkdrHash(key)&uint32((m.shardCount-1))]
+func (m *SyncMap) locate(key interface{}) *syncMap {
+	ik, ok := key.(int)
+	if ok {
+		return m.shards[uint32(ik) & uint32((m.shardCount - 1))]
+	}
+	sk := key.(string)
+	return m.shards[bkdrHash(sk) & uint32((m.shardCount - 1))]
 }
 
 // Retrieves a value
-func (m *SyncMap) Get(key string) (value interface{}, ok bool) {
+func (m *SyncMap) Get(key interface{}) (value interface{}, ok bool) {
 	shard := m.locate(key)
 	shard.RLock()
 	value, ok = shard.items[key]
@@ -59,7 +64,7 @@ func (m *SyncMap) Get(key string) (value interface{}, ok bool) {
 }
 
 // Sets value with the given key
-func (m *SyncMap) Set(key string, value interface{}) {
+func (m *SyncMap) Set(key interface{}, value interface{}) {
 	shard := m.locate(key)
 	shard.Lock()
 	shard.items[key] = value
@@ -67,7 +72,7 @@ func (m *SyncMap) Set(key string, value interface{}) {
 }
 
 // Removes an item
-func (m *SyncMap) Delete(key string) {
+func (m *SyncMap) Delete(key interface{}) {
 	shard := m.locate(key)
 	shard.Lock()
 	delete(shard.items, key)
@@ -75,16 +80,16 @@ func (m *SyncMap) Delete(key string) {
 }
 
 // Pop delete and return a random item in the cache
-func (m *SyncMap) Pop() (string, interface{}) {
+func (m *SyncMap) Pop() (interface{}, interface{}) {
 	if m.Size() == 0 {
 		panic("syncmap: map is empty")
 	}
 
 	var (
-		key   string
+		key interface{}
 		value interface{}
 		found = false
-		n     = int(m.shardCount)
+		n = int(m.shardCount)
 	)
 
 	for !found {
@@ -105,7 +110,7 @@ func (m *SyncMap) Pop() (string, interface{}) {
 }
 
 // Whether SyncMap has the given key
-func (m *SyncMap) Has(key string) bool {
+func (m *SyncMap) Has(key interface{}) bool {
 	_, ok := m.Get(key)
 	return ok
 }
@@ -127,15 +132,15 @@ func (m *SyncMap) Flush() int {
 	for _, shard := range m.shards {
 		shard.Lock()
 		size += len(shard.items)
-		shard.items = make(map[string]interface{})
+		shard.items = make(map[interface{}]interface{})
 		shard.Unlock()
 	}
 	return size
 }
 
 // Returns a channel from which each key in the map can be read
-func (m *SyncMap) IterKeys() <-chan string {
-	ch := make(chan string)
+func (m *SyncMap) IterKeys() <-chan interface{} {
+	ch := make(chan interface{})
 	go func() {
 		for _, shard := range m.shards {
 			shard.RLock()
@@ -151,7 +156,7 @@ func (m *SyncMap) IterKeys() <-chan string {
 
 // Item is a pair of key and value
 type Item struct {
-	Key   string
+	Key   interface{}
 	Value interface{}
 }
 
@@ -177,14 +182,14 @@ func bkdrHash(str string) uint32 {
 	var h uint32
 
 	for _, c := range str {
-		h = h*seed + uint32(c)
+		h = h * seed + uint32(c)
 	}
 
 	return h
 }
 
 func isPowerOfTwo(x uint8) bool {
-	return x != 0 && (x&(x-1) == 0)
+	return x != 0 && (x & (x - 1) == 0)
 }
 
 func init() {
